d2c5a34f0d31919db21004db5475fd92c4a66dff
Anjel28 <anjelchaudhary28@gmail.com>
2025-12-25 19:54:48 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+//import Counter from './components/Counter';
+import Modal from './components/Modal';
+
+const App:React.FC = () => {
+  return(
+    <div>
+      <Modal/>
+        {/* <Counter/> */}
+    </div>
+  )
+}
+
+export default App;
